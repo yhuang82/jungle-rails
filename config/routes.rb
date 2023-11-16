bb5e76b9198174df4c1133b91e6cd229ae51e55d
Yiqun Huang <albert913388672@gmail.com>
2023-11-16 10:15:05 -0800

    add the cate controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/about', to: 'about#index', as: 'about'
+  resources :about, only: [:index], path: '/about', controller: 'about'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
