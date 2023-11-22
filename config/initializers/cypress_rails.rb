7051118d8163b5c09b92a218168715c5ea41a5f1
Yiqun Huang <albert913388672@gmail.com>
2023-11-21 16:52:49 -0800

    Feature Spec: User Login

--- config/initializers/cypress_rails.rb
+++ config/initializers/cypress_rails.rb
@@ -23,6 +23,14 @@ CypressRails.hooks.before_server_start do
     quantity: 18,
     price: 24.99
   })
+
+  # user = User.create!({
+  #   first_name: 'Albert',
+  #   last_name: 'Huang',
+  #   email: 'albert@gmail.com',
+  #   password: 'password',
+  #   password_confirmation: 'password'
+  # })
 end
 
 CypressRails.hooks.after_transaction_start do
---
+++ cypress/integration/user-login.spec.js
@@ -0,0 +1,61 @@
+describe("User Create", () => {
+  it("There are products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("Creates a new user and logs in the user", () => {
+    // go to Signup page
+    // signup page
+    cy.contains("Signup").click();
+    // fill in the form and check user auth
+    cy.get('[name="user[first_name]"]').type("Albert");
+    cy.get('[name="user[last_name]"]').type("Huang");
+    cy.get('[name="user[email]"]').type("albert@gmail.com");
+    cy.get('[name="user[password]"]').type("password");
+    cy.get('[name="user[password_confirmation]"]').type("password");
+
+    cy.get("[name=commit]").click();
+    cy.contains("Hello, Albert");
+    cy.contains("Logout").click();
+
+    
+        // fill in the form and check user auth
+
+    cy.get("[id=email]").type("albert@gmail.com");
+    cy.get("[id=password]").type("password");
+    cy.get("[name=commit]").click();
+    cy.contains("Hello, Albert");
+  });
+});
+
+
+
+// describe("User Login", () => {
+//   it("There are products on the page", () => {
+//     cy.visit("/");
+//     cy.get(".products article").should("be.visible");
+//   });
+
+//   it("There are 2 products on the page", () => {
+//     cy.visit("/");
+//     cy.get(".products article").should("have.length", 2);
+//   });
+
+//   it("Logs in the user with matching email and password and redirects user to home page", () => {
+//     // go to login page
+//     cy.contains("Login").click();
+//     cy.url().should("include", "/login");
+//     // fill in the form and check user auth
+
+//     cy.get("[id=email]").type("albert@gmail.com");
+//     cy.get("[id=password]").type("password");
+//     cy.get("[name=commit]").click();
+//     cy.contains("Hello, Albert");
+//   });
+// });
